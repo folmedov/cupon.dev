3ea0da30a32f740d7184c299f93d39cad0801635
Francisco Olmedo Valencia <f.olmedo.v@gmail.com>
2014-09-14 20:28:42 -0300

    Cap-4.3 Desarrollado

---
+++ src/Cupon/OfertaBundle/Controller/SitioController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of SitioController
+ *
+ * @author francisco
+ */
+class SitioController extends Controller {
+    //put your code here
+    
+    public function estaticaAction($pagina) {
+        return $this->render('OfertaBundle:Sitio:'.$pagina.'.html.twig');
+    }
+    
+}
