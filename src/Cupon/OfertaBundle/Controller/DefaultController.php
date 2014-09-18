c93facd0aeb99fbca4ee71d60c8946b8781fd0e4
Francisco Olmedo Valencia <f.olmedo.v@gmail.com>
2014-09-18 20:42:54 -0300

    Cap-6.3 Desarrollado

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,18 +3,29 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
+    public function indexAction($name) {
         return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
     }
-    
-    public function ayudaAction()
-    {
-//        return new Response('Ayuda');
+
+    public function ayudaAction() {
         return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
+    
+    public function portadaAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
+            'ciudad'=> 1,
+            'fecha_publicacion' => new \DateTime('today')
+        ));
+
+        return $this->render(
+                        'OfertaBundle:Default:portada.html.twig', 
+                        array('oferta' => $oferta)
+        );
+    }
+
 }
